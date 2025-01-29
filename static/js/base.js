60eeda34fb8943f1d79e119a3c9282ac2efdafde
Mizael <devmizael@gmail.com>
2025-01-29 15:13:19 -0400

    Sidebar Responsiva Completa - sem conteudo principal

--- static/js/base.js
+++ static/js/base.js
@@ -1,7 +1,33 @@
 const sidebar = document.querySelector(".sidebar");
 const sidebarToggler = document.querySelector('.sidebar-toggler');
+const menuToggler = document.querySelector('.menu-toggler');
+
+const collapsedSidebarHeight = "56px";
+const fullSidebarHeight = "calc(100vh - 32px)";
 
 // Toggler sidebar's collapsed state
 sidebarToggler.addEventListener('click', () => {
     sidebar.classList.toggle('collapsed');
+});
+
+// Update sidebar height and menu toggle text
+const togglerMenu = (isMenuActive) => {
+    sidebar.style.height = isMenuActive ? `${sidebar.scrollHeight}px` : collapsedSidebarHeight;
+    menuToggler.querySelector("span").innerText = isMenuActive ? "Close" : "Menu";
+}
+
+//Toggle menu active class and adjust sidebar height
+menuToggler.addEventListener('click', () => {
+    togglerMenu(sidebar.classList.toggle("menu-active"));
+});
+
+// Adjust sidebar height on window resize
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= 1024) {
+        sidebar.style.height = fullSidebarHeight;
+    } else {
+        sidebar.classList.remove("collapsed");
+        sidebar.style.height = "auto";
+        togglerMenu(sidebar.classList.contains("menu-active"));
+    }
 });
